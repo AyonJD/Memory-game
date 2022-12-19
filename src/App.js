fc963190632b32b301b04e71ecc8249c363521e3
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-19 17:49:16 +0600

    Images initiliazed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const allImages = [
+    { id: 1, src: 'https://ibb.co/8M2gHB6' },
+    { id: 2, src: 'https://ibb.co/PxMrLbq' },
+    { id: 3, src: 'https://ibb.co/C7nTL2P' },
+    { id: 4, src: 'https://ibb.co/8NnCqrp' },
+    { id: 5, src: 'https://ibb.co/rsZ11z9' },
+    { id: 6, src: 'https://ibb.co/2KRR1KL' },
+    { id: 7, src: 'https://ibb.co/tb64tSC' },
+    { id: 8, src: 'https://ibb.co/CQRKxdj' },
+    { id: 9, src: 'https://ibb.co/gSPPBxX' },
+    { id: 10, src: 'https://ibb.co/sHn4v9L' },
+    { id: 11, src: 'https://ibb.co/3hy83FW' },
+    { id: 12, src: 'https://ibb.co/t2jXXmr' },
+    { id: 13, src: 'https://ibb.co/w0BTJZC' },
+  ];
+
+  const selectRandomFour = () => {
+    //dont repeat the same image
+    const randomFour = [];
+    while (randomFour.length < 4) {
+      const random = Math.floor(Math.random() * allImages.length);
+      if (!randomFour.includes(random)) {
+        randomFour.push(allImages[random]);
+      }
+    }
+    return randomFour;
+  };
+
+  console.log(selectRandomFour());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      OK
     </div>
   );
 }
